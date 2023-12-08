196a9104b7f18944c62d49079176f83ff4e4014b
Mushroom <mushroom@watchingyour.tv>
2023-12-08 10:54:12 +0000

    Switch to yaml stuff

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-IMAGE=mushroom/aoc-2023-bench
-
-docker image inspect "${IMAGE}" >/dev/null 2>&1 || docker build .docker -t "${IMAGE}"
-
-exec docker run --rm -i -v "${PWD}:/code" "${IMAGE}" "${@}"
